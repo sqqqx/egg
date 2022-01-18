986379b2b236618814cc8c97a294ee3eba4502b9
Hojin <yooho20044@naver.com>
2022-01-18 12:21:16 +0900

    modify MemberController

--- src/main/java/egg/finalproject/member/MemberController.java
+++ src/main/java/egg/finalproject/member/MemberController.java
@@ -1,14 +1,8 @@
 package egg.finalproject.member;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping("/member")
 public class MemberController {
 
-	@RequestMapping("/toMypage.do")
-	public String toMypage() {
-		return "/member/mypage";
-	}
+	
+	
 }
