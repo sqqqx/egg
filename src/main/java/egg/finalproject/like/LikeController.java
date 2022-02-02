ef8e4415abccc30c4831022d047fe69e1fab4b04
Hojin <yooho20044@naver.com>
2022-02-02 17:47:31 +0900

    add Like

---
+++ src/main/java/egg/finalproject/like/LikeController.java
@@ -0,0 +1,63 @@
+package egg.finalproject.like;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "/like")
+public class LikeController {
+	
+	@Autowired
+	private LikeService service;
+	@Autowired
+	private HttpSession session;
+	
+	//좋아요 카운트
+	@RequestMapping("/count.do")
+	@ResponseBody
+	public int count(int post_no) throws Exception {
+		int count = service.count(post_no);
+		return count;
+	}
+	
+	//좋아요 추가
+	@RequestMapping("/plus.do")
+	@ResponseBody
+	public String plus(String user_id, int post_no) throws Exception{
+		if(service.plus(user_id, post_no) ==1) {
+			return "available";
+		}else {
+			return "unavailable";
+		}
+		
+	}
+	
+	//좋아요 취소
+	@RequestMapping("/minus.do")
+	@ResponseBody
+	public String minus(String user_id, int post_no) throws Exception{
+		if(service.minus(user_id, post_no) ==1) {
+			return "available";
+		}else {
+			return "unavailable";
+		}
+		
+	}
+	
+	//유저가 게시글에 좋아요 눌렀는지 판별
+	@RequestMapping("/selectLike.do")
+	@ResponseBody
+	public String selectLike(String user_id, int post_no) throws Exception{
+		if(service.selectLike(user_id, post_no) ==1) {
+			return "available";
+		}else {
+			return "unavailable";
+		}
+		
+	}
+
+}
---
+++ src/main/java/egg/finalproject/like/LikeService.java
@@ -0,0 +1,28 @@
+package egg.finalproject.like;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LikeService {
+
+	@Autowired
+	private LikeDAO dao;
+	
+	public int count(int post_no) throws Exception{
+		return dao.count(post_no);
+	}
+	
+	public int plus(String user_id, int post_no) throws Exception{
+		return dao.plus(user_id, post_no);
+	}
+	
+	public int minus(String user_id, int post_no) throws Exception{
+		return dao.minus(user_id, post_no);
+	}
+	
+	public int selectLike(String user_id, int post_no) throws Exception{
+		return dao.selectLike(user_id, post_no);
+			
+	}
+}
