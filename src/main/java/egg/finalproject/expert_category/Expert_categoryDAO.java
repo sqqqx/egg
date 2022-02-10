f65968a5ac01131ca358c2c3eee1c4abefacc961
dhkim <ctrlcvv@gmail.com>
2022-02-10 10:04:55 +0900

    conflict clear

---
+++ src/main/java/egg/finalproject/expert_category/Expert_categoryDAO.java
@@ -0,0 +1,30 @@
+package egg.finalproject.expert_category;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class Expert_categoryDAO {
+	
+	@Autowired
+	private SqlSession session;
+
+	public List<Object> ExpertCategory(String expert_id) throws Exception{
+		return session.selectList("expertcategoryMapper.ExpertCategory", expert_id);
+	}
+	
+	// (마이페이지) 능력자 카테고리 번호 추가
+	public int insertExpertCN(Expert_categoryDTO dto) throws Exception {
+		System.out.println("Expert_categoryDAO / 능력자 카테고리 번호 추가 - dto: " + dto);
+		return session.insert("expertcategoryMapper.insertExpertCN", dto);
+	}
+	
+	// (마이페이지) 카테고리 번호 가져오기
+	public List<Integer> noList(String expert_id) throws Exception {
+		System.out.println("Expert_categoryDAO / 카테고리 번호 가져오기 - expert_id: " + expert_id);
+		return session.selectList("expertcategoryMapper.noList", expert_id);
+	}
+}
